0e4a91ae33ad335423901dd478c293906cba230a
senne <sennebels@gmail.com>
2024-01-12 11:34:52 +0100

    feat: :sparkles: project init

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
-
+    '@appuniversum/ember-appuniversum': {
+      disableWormholeElement: true,
+    },
+    sassOptions: {
+      includePaths: ['node_modules/@appuniversum/ember-appuniversum'], // just "node_modules" would also work, but it seems to slow (re)builds down a lot
+    },
     // Add options here
   });
 
