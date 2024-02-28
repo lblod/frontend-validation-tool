8f99eab533cbcc5a507cac2992bd250ede277a88
snenenenenenene <sennebels@gmail.com>
2024-02-28 15:18:58 +0100

    added meta

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,32 @@
 
 module.exports = function (environment) {
   const ENV = {
+    'ember-meta': {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      msapplication: {
+        square70x70logo:
+          'https://ui.vlaanderen.be/2.latest/icons/app-icon/tile-small.png',
+        square150x150logo:
+          'https://ui.vlaanderen.be/2.latest/icons/app-icon/tile-medium.png',
+        wide310x150logo:
+          'https://ui.vlaanderen.be/2.latest/icons/app-icon/tile-wide.png',
+        square310x310logo:
+          'https://ui.vlaanderen.be/2.latest/icons/app-icon/tile-large.png',
+        TileColor: '#FFED00',
+        'application-name': 'Vlaanderen',
+      },
+      'og:title': 'Validation Monitoring Tool',
+      'twitter:card': 'summary_large_image',
+
+      'twitter:title': ' Validation Monitoring Tool',
+      'twitter:site': '@info_vlaanderen',
+      'og:type': 'website',
+      'og:description':
+        'Concept for a tool to validate publications for harvesting. When multiple publishers are publishing data to a triple store, this tool can be used to validate the data against a blueprint of said data.',
+      'twitter:description':
+        'Concept for a tool to validate publications for harvesting. When multiple publishers are publishing data to a triple store, this tool can be used to validate the data against a blueprint of said data.',
+    },
     modulePrefix: 'validation-monitoring-tool',
     environment,
     rootURL: '/',
