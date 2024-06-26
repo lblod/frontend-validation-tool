4f78f8d05d7a978726ab377bd4a89760def610b5
snenenenenenene <sennebels@gmail.com>
2024-06-26 09:56:29 +0200

    added whitelist

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    dotEnv: {
+      clientAllowedKeys: ['CORS_PROXY_URL'],
+    },
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
     },
