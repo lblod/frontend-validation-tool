4355243fae1906099fed595d6eea99de8e0a0ac6
snenenenenenene <sennebels@gmail.com>
2024-06-27 11:05:12 +0200

    added new proxy url

--- config/environment.js
+++ config/environment.js
@@ -35,7 +35,9 @@ module.exports = function (environment) {
     EmberENV: {
       EXTEND_PROTOTYPES: false,
     },
-    CORS_PROXY_URL: process.env.CORS_PROXY_URL || 'http://localhost:3000',
+    CORS_PROXY_URL:
+      process.env.CORS_PROXY_URL ||
+      'https://validatietool.lblod.info/cors?url=',
 
     APP: {
       // Here you can pass flags/options to your application instance
