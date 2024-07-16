13346f30c2ac00bc479cacbf69847ac8cf2ad038
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-16 15:22:09 +0200

    BnB-699: add a fallback for the proxy to be set to the ENV variable

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -3,14 +3,19 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 import type DocumentService from 'frontend-validation-tool/services/document';
+import ENV from '../config/environment';
 
 export default class ApplicationRoute extends Route {
   @service declare document: DocumentService;
 
   model() {
-    const origin = window.location.origin;
-    const dispatchPath = '/communica';
+    if (!ENV.CORS_PROXY_URL) {
+      const origin = window.location.origin;
+      const dispatchPath = '/communica';
+      this.document.setCorsProxy(`${origin}${dispatchPath}?url=`);
+      return;
+    }
 
-    this.document.setCorsProxy(`${origin}${dispatchPath}?url=`);
+    this.document.setCorsProxy(ENV.CORS_PROXY_URL);
   }
 }
