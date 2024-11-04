b8ae7de92d076c63547c205282cb878b2e6adf2c
brechtvdv <br5cht@hotmail.com>
2024-11-04 12:25:54 +0100

    Not working

---
+++ tests/test-helper.ts
@@ -0,0 +1,12 @@
+import Application from 'frontend-validation-tool/app';
+import config from 'frontend-validation-tool/config/environment';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
+import { setup } from 'qunit-dom';
+import * as QUnit from 'qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
