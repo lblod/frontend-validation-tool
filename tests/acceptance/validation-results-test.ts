bc08b1434861309149e9a0b759fbaf4faef52d1c
brechtvdv <br5cht@hotmail.com>
2024-11-04 15:57:13 +0100

    Update test of validation-results

--- tests/acceptance/validation-results-test.ts
+++ tests/acceptance/validation-results-test.ts
@@ -1,17 +1,16 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, pauseTest, currentRouteName } from '@ember/test-helpers';
 import { setupApplicationTest } from 'frontend-validation-tool/tests/helpers';
 
 module('Acceptance | validation results', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /validation-results', async function (assert) {
+  test('visiting /validation-results without query params should lead to document-upload route', async function (assert) {
     try {
-      await visit('/validation-results', { accept: 'text/html' });
+      await visit('/validation-results');
     } catch (e) {
       console.error(e);
     }
-
-    assert.strictEqual(currentURL(), '/');
+    assert.strictEqual(currentRouteName(), 'document-upload');
   });
 });
