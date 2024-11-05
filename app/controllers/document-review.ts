2c5743e8f7e00c7a64cbcaead3dc9e3d139c60d7
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-11-05 13:56:42 +0100

    Add upload box for custom shacl rules

--- app/controllers/document-review.ts
+++ app/controllers/document-review.ts
@@ -5,10 +5,18 @@ import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import type DocumentService from 'frontend-validation-tool/services/document';
 import { getDocumentTypes } from '@lblod/lib-decision-validation/dist/queries';
+import type { UploadFile } from 'ember-file-upload/upload-file';
 
 export default class DocumentReviewController extends Controller {
   @service declare document: DocumentService;
   @service declare router: RouterService;
+
+  @tracked selectOptions = getDocumentTypes();
+  @tracked uploadedFile: File | null = null;
+
+  @action onFinishUpload(uploadedFile: UploadFile) {
+    this.uploadedFile = uploadedFile.file;
+  }
 
   @action handleValidation() {
     this.router.transitionTo('validation-results', {
@@ -18,6 +26,4 @@ export default class DocumentReviewController extends Controller {
       },
     });
   }
-
-  @tracked selectOptions = getDocumentTypes();
 }
