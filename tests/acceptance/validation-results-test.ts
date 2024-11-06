bc884a5dcd9d47f737767e572f4025c479fc0a02
brechtvdv <br5cht@hotmail.com>
2024-11-06 16:23:32 +0100

    Add test that transitions between all routes

--- tests/acceptance/validation-results-test.ts
+++ tests/acceptance/validation-results-test.ts
@@ -1,4 +1,14 @@
-import { visit, currentRouteName, waitUntil } from '@ember/test-helpers';
+import {
+  visit,
+  currentRouteName,
+  currentURL,
+  waitUntil,
+  fillIn,
+  click,
+  pauseTest,
+  settled,
+  waitFor,
+} from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'frontend-validation-tool/tests/helpers';
 import 'qunit-dom';
@@ -6,8 +16,10 @@ import 'qunit-dom';
 module('Acceptance | validation results', function (hooks) {
   setupApplicationTest(hooks);
 
+  const EXAMPLE_URL = 'http://localhost:4200/example-notulen.html';
+
   test('visiting /example-notulen returns HTML with Notulen mentioned', async function (assert) {
-    const res = await fetch('http://localhost:4200/example-notulen.html');
+    const res = await fetch(EXAMPLE_URL);
     const text = await res.text();
     assert.ok(text.includes('<!DOCTYPE html>'));
     assert.ok(text.includes('Notulen'));
@@ -25,13 +37,13 @@ module('Acceptance | validation results', function (hooks) {
   });
 
   test('visiting /validation-results with query params defined should return validation results route', async function (assert) {
-    const url = `/validation-results?documentType=Notulen&url=${encodeURIComponent('http://localhost:4200/example-notulen.html')}`;
+    const url = `/validation-results?documentType=Notulen&url=${encodeURIComponent(EXAMPLE_URL)}`;
     await visit(url);
     assert.strictEqual(currentRouteName(), 'validation-results');
   });
 
   test('visiting /validation-results to validate example-notulen.html should return 2 Class Collection components and 7 Root Subject components', async function (assert) {
-    const url = `/validation-results?documentType=Notulen&url=${encodeURIComponent('http://localhost:4200/example-notulen.html')}`;
+    const url = `/validation-results?documentType=Notulen&url=${encodeURIComponent(EXAMPLE_URL)}`;
     await visit(url);
 
     const controller = this.owner.lookup('controller:validation-results');
@@ -58,5 +70,58 @@ module('Acceptance | validation results', function (hooks) {
         { count: 7 },
         'The Root Subject component is rendered seven times',
       );
+  });
+
+  test('transition between document-upload, document-review, and validation-results', async function (assert) {
+    // Step 1: Visit the document-upload route
+    await visit('/');
+    assert.strictEqual(
+      currentURL(),
+      '/',
+      'We are on the document-upload route',
+    );
+    assert
+      .dom('[data-test-document-upload]')
+      .exists('Document-upload page is displayed');
+
+    // Fill in example URL
+    await fillIn('[data-test-field="publication-link"]', EXAMPLE_URL);
+
+    // Simulate transition to document-review by clicking "Volgende" button to proceed
+    await click('[data-test-button="volgende"]');
+
+    await waitFor('[data-test-document-review]', { timeout: 15000 }); // Wait for an element in the document-review route
+
+    // Step 2: Transition to document-review route
+    assert.strictEqual(
+      currentRouteName(),
+      'document-review',
+      'We transitioned to the document-review route',
+    );
+    assert
+      .dom('[data-test-document-review]')
+      .exists('Document-review page is displayed');
+
+    assert
+      .dom('[data-test-field="documentTypeSelect"] option:checked')
+      .hasText('Notulen', 'The selected option is "Notulen"');
+
+    // Simulate transition to validation-results by clicking "Validatie starten" button
+    await click('[data-test-button="validatieStarten"]');
+
+    // Step 3: Transition to validation-results route
+    assert.strictEqual(
+      currentRouteName(),
+      'validation-results',
+      'We transitioned to the validation-results route',
+    );
+    assert
+      .dom('[data-test-validation-results]')
+      .exists('Validation-results page is displayed');
+
+    // Assert that the publication link contains the correct href and text
+    assert
+      .dom('[data-test-field="publication-link"]')
+      .hasAttribute('href', EXAMPLE_URL, 'Link has correct href');
   });
 });
